7d7f5e1b435a2bdaf763e77e8f99767673cef6df
Joel Hashman <jhashman@ntst.com>
2017-09-28 22:47:48 -0400

    Updated file locations"

--- nyap.py
+++
@@ -1,9 +0,0 @@
-# nethydra.py
-nethydra_input = '/home/nocops/scripts/nethydra/input/nyap-devices.csv'
-username1 = 'nocops'
-password1 = 'Cyberh3lp'
-username2 = 'nocops'
-username2 = 'Cyber780'
-enable_pass = 'Cyberh3lp'
-ssh_config_file = '/home/nocops/.ssh/config'
-
